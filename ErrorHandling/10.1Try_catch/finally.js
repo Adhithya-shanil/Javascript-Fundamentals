9c2395457f3bcbfb0f657abfd8eec94ea6f662b7
Adhithya-shanil <adhithya.s@neoito.com>
2025-04-02 11:40:33 +0530

    async function and promise

---
+++ ErrorHandling/10.1Try_catch/finally.js
@@ -0,0 +1,19 @@
+// #The primary advantage of using the finally block is that it ensures the cleanup code always runs, 
+// regardless of whether an error occurs in the try block or not. This is especially useful for handling resources like file streams, 
+// network connections, or database transactions, where the cleanup is critical to avoid resource leaks.
+
+// try {
+//   work work
+// } catch (err) {
+//   handle errors
+// } finally {
+//   cleanup the working space
+// }
+
+// try {
+//   work work
+// } catch (err) {
+//   handle errors
+// }
+
+// cleanup the working space
